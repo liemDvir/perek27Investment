9e3bb4c7382ab4f91095b589b59e51e20de33f58
liemDvir <dvir.liem@gmail.com>
2025-03-02 00:19:34 +0200

    change name

--- app/src/main/java/com/example/perek27/Observer.java
+++ app/src/main/java/com/example/perek27/Observer.java
@@ -5,7 +5,7 @@ import androidx.annotation.NonNull;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 
-public interface Observer {
+public interface IObserver {
 
     public void SignInWithEmailAndPasswordCompleate(@NonNull Task<AuthResult> task);
 }
