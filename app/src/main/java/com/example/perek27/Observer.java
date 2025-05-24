9970d361cb8921f1ab677c061f7b918f9e10ae2c
liemDvir <dvir.liem@gmail.com>
2025-05-24 20:01:33 +0300

    change to stockInfo

--- app/src/main/java/com/example/perek27/Observer.java
+++ app/src/main/java/com/example/perek27/Observer.java
@@ -6,20 +6,19 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public interface Observer {
 
     public void SignInWithEmailAndPasswordCompleate(@NonNull Task<AuthResult> task);
 
-    public void GetAllStocksInvested(List<Stock> stockInvested);
+    public void GetAllStocksInvested(List<StockInfo> stockInvested);
 
     public void GetTransactionHistory(ArrayList<Transaction> transactionsList);
 
-    public void GetAllStocksInMarket(List<Stock> stocksList);
+    public void GetAllStocksInMarket(List<StockInfo> stocksList);
 
-    public void getALLCash(float cash);
+    public void GetAllCash(float cash);
 
     public void GetAllUserData(UserData userData);
 
