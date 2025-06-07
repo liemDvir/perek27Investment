ba236ac3f6463e06230c34087eb45e5cb7e69c31
liemDvir <dvir.liem@gmail.com>
2025-06-07 18:07:28 +0300

    fix BuyStock function

--- app/src/main/java/com/example/perek27/StockModel.java
+++ app/src/main/java/com/example/perek27/StockModel.java
@@ -1,21 +1,32 @@
 package com.example.perek27;
 
+import android.app.AlarmManager;
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class StockModel extends Application {
@@ -168,53 +179,229 @@ public class StockModel extends Application {
 
         mModelObserver.GetAllCash(mUserData.getCash());
     }
+
+
     public void BuyStock(String stockSymbol, float amountOfMoneyToBuy)
     {
         if(mStockControllerService == null)
             return ;
 
-        if(mUserData.getCash() < amountOfMoneyToBuy){
-            Log.e("BuyStock()", "Not enough money");
-            mModelObserver.OnBuyStockCompleted(false, "Not enough money");
-            return;
-        }
+        Log.d("StockModel", "BuyStock - stockSymbol:" + stockSymbol);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if(mUserData == null){
+                    mStockControllerService.GetAllUserData();
+                    synchronized (lockObject){
+                        try {
+                            if(mUserData == null) {
+                                lockObject.wait(10000);
+                            }
+                        } catch (InterruptedException e) {
+                            mModelObserver.OnBuyStockCompleted(false, "Timeout");
+                            return;
+                        }
+                    }
+                }
 
-        //Update stock value
-        GetStockInfo(stockSymbol);
-        synchronized (lockObject){
-            try {
-                lockObject.wait(10000);
-            } catch (InterruptedException e) {
-                mModelObserver.OnBuyStockCompleted(false, "Timeout");
-                return;
+                if(mUserData.getCash() < amountOfMoneyToBuy){
+                    Log.e("BuyStock()", "Not enough money");
+                    mModelObserver.OnBuyStockCompleted(false, "Not enough money");
+                    return;
+                }
+
+                //Update cash on datebase
+                mStockControllerService.UpdateCash(mUserData.getCash() - amountOfMoneyToBuy);
+                synchronized (lockObject){
+                    try {
+                        lockObject.wait(10000);
+                    } catch (InterruptedException e) {
+                        mModelObserver.OnBuyStockCompleted(false, "Connection timeout");
+                        return;
+                    }
+                }
+
+                if(!bUpdateCashSuccess){
+                    synchronized (lockObject){
+                        mModelObserver.OnBuyStockCompleted(false, "Update failure");
+                    }
+                    return;
+                }
+
+                mUserData.setCash(mUserData.getCash() - amountOfMoneyToBuy);
+                //Update cash on GUI
+                mModelObserver.GetAllCash(mUserData.getCash());
+
+                float amoutOfStockToBuy = 0;
+                StockInfo newStockInfoList = null;
+                Boolean bIsNewStock = false;
+
+                //Update stocks invested
+                synchronized (mStocksInvestedList) {
+                    for (StockInfo stockInfFromList : mStocksInvestedList) {
+                        if (!stockInfFromList.getStockSymbol().equals(stockSymbol)) {
+                            continue;
+                        }
+                        newStockInfoList = stockInfFromList;
+                        break;
+                        /*float stockPrice = stockInfFromList.getPrice();
+                        if(stockPrice == 0){
+                            Log.e("StockModel", "BuyStock: stockPrice == 0");
+                            break;
+                        }
+                        amoutOfStockToBuy = amountOfMoneyToBuy/stockPrice;
+                        float currentAmountOfStocks = stockInfFromList.getAmountOfStock();
+                        stockInfFromList.setAmountOfStock(amoutOfStockToBuy + currentAmountOfStocks);
+                        mStockControllerService.UpdateAmountOfStockInvested(stockInfFromList);
+                        break;*/
+                    }
+                }
+                if((newStockInfoList == null) || (newStockInfoList.getPrice() == (float)0)){
+                    //A new invested stock
+                    bIsNewStock = true;
+                    newStockInfoList = new StockInfo("",stockSymbol,0,"0");
+                    synchronized (mStocksInvestedList){
+                        mStocksInvestedList.add(newStockInfoList);
+                    }
+                    mStockControllerService.GetStockInfo(stockSymbol);
+                    synchronized (lockObject){
+                        try {
+                                lockObject.wait(10000);
+                        } catch (InterruptedException e) {
+                            mModelObserver.OnBuyStockCompleted(false, "Timeout");
+                            return;
+                        }
+                    }
+                }
+
+                if(newStockInfoList.getPrice() == 0){
+                    Log.e("StockModel", "BuyStock: stockPrice == 0");
+                    mModelObserver.OnBuyStockCompleted(false, "Timeout");
+                    return;
+                }
+                amoutOfStockToBuy = amountOfMoneyToBuy/newStockInfoList.getPrice();
+
+                if(!bIsNewStock) {
+                    amoutOfStockToBuy = newStockInfoList.getAmountOfStock() + amoutOfStockToBuy;
+                }
+                newStockInfoList.setAmountOfStock(amoutOfStockToBuy);
+                if(bIsNewStock){
+                    mStockControllerService.AddNewStockInvested(newStockInfoList);
+                }
+                else{
+                    mStockControllerService.UpdateAmountOfStockInvested(newStockInfoList);
+                }
+
+
+                synchronized (lockObject){
+                    for (final Observer observer : mObservers) {
+                        observer.GetAllStocksInvested(mStocksInvestedList);
+                    }
+                }
+
+                //Update transaction history
+                SetTransaction(new Transaction((int)amountOfMoneyToBuy,stockSymbol,((-1)*amoutOfStockToBuy), Date.from(Instant.now())));
+
+                synchronized (lockObject){
+                    mModelObserver.OnBuyStockCompleted(true, "Action completed");
+                }
+
             }
-        }
+        });
 
-        //Update cash on datebase
-        mStockControllerService.UpdateCash(mUserData.getCash() - amountOfMoneyToBuy);
-        synchronized (lockObject){
-            try {
-                lockObject.wait(10000);
-            } catch (InterruptedException e) {
-                mModelObserver.OnBuyStockCompleted(false, "update");
-                return;
-            }
-        }
-
-        //Update transaction history
-        //TODO
-
-        //Update cash
-        //TODO
-
-        //Update stocks invested
-        //TODO
-
+        thread.start();
     }
 
     public void SellStock(String stockSymbol, float amountOfMoneyToSell)
     {
+        if(mStockControllerService == null)
+            return ;
 
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if(mUserData == null){
+                    mStockControllerService.GetAllUserData();
+                    synchronized (lockObject){
+                        try {
+                            if(mUserData == null) {
+                                lockObject.wait(10000);
+                            }
+                        } catch (InterruptedException e) {
+                            mModelObserver.OnBuyStockCompleted(false, "Timeout");
+                            return;
+                        }
+                    }
+                }
+
+                StockInfo stockToSell = null;
+                synchronized (mStocksInvestedList){
+                    for(StockInfo stockInfFromList : mStocksInvestedList){
+                        if(!stockInfFromList.getStockSymbol().equals(stockSymbol)){
+                            continue;
+                        }
+                        stockToSell = stockInfFromList;
+                        float stockPrice = stockInfFromList.getPrice();
+                        float amountOfStock = stockInfFromList.getAmountOfStock();
+                        if(amountOfMoneyToSell > (amountOfStock * stockPrice)){
+                            mModelObserver.OnBuyStockCompleted(false, "Not enough stocks to sell");
+                            return;
+                        }
+                    }
+                }
+
+
+                //Update cash on datebase
+                mStockControllerService.UpdateCash(mUserData.getCash() + amountOfMoneyToSell);
+                synchronized (lockObject){
+                    try {
+                        lockObject.wait(10000);
+                    } catch (InterruptedException e) {
+                        mModelObserver.OnBuyStockCompleted(false, "Connection timeout");
+                        return;
+                    }
+                }
+
+                if(!bUpdateCashSuccess){
+                    synchronized (lockObject){
+                        mModelObserver.OnBuyStockCompleted(false, "Update failure");
+                    }
+                    return;
+                }
+
+                mUserData.setCash(mUserData.getCash() + amountOfMoneyToSell);
+                //Update cash on GUI
+                mModelObserver.GetAllCash(mUserData.getCash());
+
+
+                float stockPrice = stockToSell.getPrice();
+                float amoutOfStockToSell = amountOfMoneyToSell/stockPrice;
+                float currentAmountOfStocks = stockToSell.getAmountOfStock();
+
+                //Update stocks invested
+                synchronized (mStocksInvestedList){
+                    stockToSell.setAmountOfStock(currentAmountOfStocks - amoutOfStockToSell);
+                    mStockControllerService.UpdateAmountOfStockInvested(stockToSell);
+                }
+
+                synchronized (lockObject){
+                    for (final Observer observer : mObservers) {
+                        observer.GetAllStocksInvested(mStocksInvestedList);
+                    }
+                }
+
+                //Update transaction
+                amoutOfStockToSell = (amoutOfStockToSell * (-1));
+                SetTransaction(new Transaction((int)amoutOfStockToSell,stockToSell.getStockSymbol(),amountOfMoneyToSell, Date.from(Instant.now())));
+
+                synchronized (lockObject){
+                    mModelObserver.OnSellStockCompleted(true, "Action completed");
+                }
+
+            }
+        });
+
+        thread.start();
 
     }
     public LineGraphSeries<DataPoint> GetCurrentStockGraphViewSeries(Stock stock)
@@ -257,6 +444,7 @@ public class StockModel extends Application {
         if(mStockControllerService == null)
             return;
 
+        Log.d("StockModel", "GetStockInfo: " + stockSymbol);
         mStockControllerService.GetStockInfo(stockSymbol);
     }
 
@@ -272,6 +460,13 @@ public class StockModel extends Application {
                 return;
             }
             mStockControllerService.register(mModelObserver);
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, 23);
+            calendar.set(Calendar.MINUTE, 56);
+            calendar.set(Calendar.SECOND, 0);
+
+            setExactAlarm(getApplicationContext(), calendar);
            /* for (final Observer observer : mObservers) {
                 register(observer);
             }*/
@@ -314,7 +509,11 @@ public class StockModel extends Application {
 
         @Override
         public void GetTransactionHistory(ArrayList<Transaction> transactionsList) {
-
+            synchronized (lockObject){
+                for (final Observer observer : mObservers) {
+                    observer.GetTransactionHistory(transactionsList);
+                }
+            }
         }
 
         @Override
@@ -347,25 +546,34 @@ public class StockModel extends Application {
                 for (final Observer observer : mObservers) {
                     observer.GetAllUserData(userDate);
                 }
+                lockObject.notifyAll();
             }
 
         }
         @Override
         public void OnStockInfoUpdate(StockInfo stockInf) {
+            if(stockInf == null){
+                Log.e("StockModel", "OnStockInfoUpdate complete with error");
+                return;
+            }
+            Log.d("StockModel", "OnStockInfoUpdate complete. price:" + String.valueOf(stockInf.getPrice()));
             synchronized (mStocksInvestedList){
                 for(StockInfo stockInfFromList : mStocksInvestedList){
                     if(stockInfFromList.getStockSymbol().equals(stockInf.getStockSymbol())){
                         stockInfFromList.setPrice(stockInf.getPrice());
                         stockInfFromList.setChange_percent(stockInf.getChange_percent());
+                        if((stockInf.getStockName() != null) && (!stockInf.getStockName().isEmpty())){
+                            stockInfFromList.setStockName(stockInf.getStockName());
+                        }
                     }
                 }
             }
 
             synchronized (lockObject){
+                Log.d("StockModel", "OnStockInfoUpdate() - notify observers");
                 for (final Observer observer : mObservers) {
                     observer.OnStockInfoUpdate(stockInf);
                 }
-
                 lockObject.notifyAll();
             }
         }
@@ -380,8 +588,56 @@ public class StockModel extends Application {
         }
 
         @Override
-        public void OnUpdateCashCompleted(Boolean success) {
+        public void OnSellStockCompleted(Boolean seccess, String reason) {
+            synchronized (lockObject){
+                for (final Observer observer : mObservers) {
+                    observer.OnSellStockCompleted(seccess,reason);
+                }
+            }
+        }
 
+        @Override
+        public void OnUpdateCashCompleted(Boolean success) {
+            bUpdateCashSuccess = success;
+            synchronized (lockObject){
+                lockObject.notifyAll();
+            }
+        }
+    }
+
+    public static class AlarmReceiver  extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("OnReceive", "time completed");
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            String channelId = "alarm_channel";
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel(channelId, "Alarm Notification", NotificationManager.IMPORTANCE_HIGH);
+                notificationManager.createNotificationChannel(channel);
+            }
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setContentTitle("Alarm")
+                    .setContentText("Your scheduled alarm went off!")
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setAutoCancel(true);
+
+            notificationManager.notify(1, builder.build());
+        }
+    }
+
+    public void setExactAlarm(Context context,Calendar calendar) {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        } else {
+            alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
         }
     }
 }
